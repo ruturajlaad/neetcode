7ead797e944ebc7e06fe3a65d8aeb34f93645b1d
Gilfoyle <108115616+ruturajlaad@users.noreply.github.com>
2025-06-11 16:36:28 +0530

    Create 4.Anagram.py

---
+++ 4.Anagram.py
@@ -0,0 +1,28 @@
+#sol1
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        if (len(s) != len(t)):
+            return False
+
+        countS,countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]]= 1+ countS.get(s[i],0)
+            countT[t[i]]= 1+ countT.get(t[i],0)
+        for c in countS:
+            if countS[c]!= countT.get(c,0):
+                return False
+        return True
+#sol2
+
+class Solution(object):
+    def isAnagram(self, s, t):
+      return Counter(s)==Counter(t)
+
+
+#sol3
+
+class Solution(object):
+    def isAnagram(self, s, t):
+      return sorted(s)== sorted(t)
